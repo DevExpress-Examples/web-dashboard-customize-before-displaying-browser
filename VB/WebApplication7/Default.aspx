9d2a2e9dc5493372d30ec79746f89cdc97ff1cb3
DevExpressExampleBot <fakeEmail>
2021-10-28 16:38:17 +0300

    autogenerate VB

--- VB/WebApplication7/Default.aspx
+++ VB/WebApplication7/Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="WebApplication7.Default" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication7.Default" %>
 
 <%@ Register Assembly="DevExpress.Dashboard.v18.2.Web.WebForms, Version=18.2.15.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.DashboardWeb" TagPrefix="dx" %>
 
--- VB/WebApplication7/Default.aspx.vb
+++ VB/WebApplication7/Default.aspx.vb
@@ -1,37 +1,39 @@
-﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon
 Imports DevExpress.DashboardWeb
 Imports DevExpress.DataAccess.Sql
 Imports System
-Imports System.Linq
 
 Namespace WebApplication7
-	Partial Public Class [Default]
-		Inherits System.Web.UI.Page
 
-		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-			ASPxDashboard1.AllowExecutingCustomSql = True
-			ASPxDashboard1.SetDashboardStorage(New DashboardFileStorage("~/App_Data/Dashboards"))
-		End Sub
+    Public Partial Class [Default]
+        Inherits Web.UI.Page
 
-		Protected Sub ASPxDashboard1_DashboardLoading(ByVal sender As Object, ByVal e As DevExpress.DashboardWeb.DashboardLoadingWebEventArgs)
-			If e.DashboardId = "dashboard0" Then
-				Dim dashboard As New Dashboard()
-				dashboard.LoadFromXDocument(e.DashboardXml)
-				' customization code
-				Dim parameterCountry = dashboard.Parameters.FirstOrDefault(Function(p) p.Name = "CountryDashboardParameter")
-				If parameterCountry IsNot Nothing Then
-					parameterCountry.Value = "Germany"
-				End If
-				Dim nwindDataSource = dashboard.DataSources.OfType(Of DashboardSqlDataSource)().FirstOrDefault(Function(ds) ds.Name = "NwindDataSource")
-				If nwindDataSource IsNot Nothing Then
-					Dim customNwindQuery = nwindDataSource.Queries.OfType(Of CustomSqlQuery)().FirstOrDefault(Function(q) q.Name = "CustomInvoicesQuery")
-					If customNwindQuery IsNot Nothing Then
-						customNwindQuery.Sql &= " AND (Invoices.OrderDate >= #2015-01-01 00:00:00#)"
-					End If
-				End If
-				'...
-				e.DashboardXml = dashboard.SaveToXDocument()
-			End If
-		End Sub
-	End Class
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ASPxDashboard1.AllowExecutingCustomSql = True
+            ASPxDashboard1.SetDashboardStorage(New DashboardFileStorage("~/App_Data/Dashboards"))
+        End Sub
+
+        Protected Sub ASPxDashboard1_DashboardLoading(ByVal sender As Object, ByVal e As DevExpress.DashboardWeb.DashboardLoadingWebEventArgs)
+            If e.DashboardId Is "dashboard0" Then
+                Dim dashboard As Dashboard = New Dashboard()
+                dashboard.LoadFromXDocument(e.DashboardXml)
+                ' customization code
+                Dim parameterCountry = dashboard.Parameters.FirstOrDefault(Function(p) p.Name Is "CountryDashboardParameter")
+                If parameterCountry IsNot Nothing Then
+                    parameterCountry.Value = "Germany"
+                End If
+
+                Dim nwindDataSource = dashboard.DataSources.OfType(Of DashboardSqlDataSource)().FirstOrDefault(Function(ds) ds.Name Is "NwindDataSource")
+                If nwindDataSource IsNot Nothing Then
+                    Dim customNwindQuery = nwindDataSource.Queries.OfType(Of CustomSqlQuery)().FirstOrDefault(Function(q) q.Name Is "CustomInvoicesQuery")
+                    If customNwindQuery IsNot Nothing Then
+                        customNwindQuery.Sql += " AND (Invoices.OrderDate >= #2015-01-01 00:00:00#)"
+                    End If
+                End If
+
+                '...
+                e.DashboardXml = dashboard.SaveToXDocument()
+            End If
+        End Sub
+    End Class
 End Namespace
